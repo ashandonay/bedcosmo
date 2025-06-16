100bb736e9e5e2389bf64eb3bb04abf2701dc723
ashandonay <ashandonay@yahoo.com>
2025-06-16 16:02:21 -0700

    updated entropy scripts

--- scripts/entropy/train.sh
+++ scripts/entropy/train.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 export OMP_NUM_THREADS=32
-GPUS=2
 
+GPUS=2
+n_particles_per_device=1000
 torchrun \
     --nproc_per_node=$GPUS \
     num_tracers/n_tracers_train_distributed.py \
     --cosmo_model base \
-    --exp_name ddp_test \
-    --n_particles_per_device 500 \
-    --steps 500 \
+    --exp_name base_NAF \
+    --n_particles_per_device $n_particles_per_device \
+    --steps 10000 \
     --pyro_seed 1 \
+    --final_lr 0.0001 \
     --verbose
